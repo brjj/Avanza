e292d9fc3fe36929c8dc21c4aaa0dfc84ca5f101
brjj <43045003+brjj@users.noreply.github.com>
2018-09-06 20:34:43 +0200

    Update avanza.py
    Tog bort onödig kod och gjorde koden mer dynamisk.

--- avanza.py
+++ avanza.py
@@ -1,19 +1,31 @@
-
 import csv;
 from datetime import datetime;
 from urllib.request import urlopen;
 from bs4 import BeautifulSoup;
-import os
-os.chmod("D:\program\python", 0o777);
+
+#----------------------------------------------------------
+quote_page = "" #Skriv URL här
+#t.ex:
+#https://www.avanza.se/fonder/om-fonden.html/788395/avanza-auto-1
+
+
+csv_path = "" #mapp att spara .csv filen i
+#t.ex:
+#D:\program\python\index.csv
+
+
+#----------------------------------------------------------
 
 def convertString(string):
     x,y = string.split(",");
     string = x + "." + y;
     return float(string);
 
-
-quote_page = "https://www.avanza.se/fonder/om-fonden.html/788395/avanza-auto-1"
-page = urlopen(quote_page);
+try:
+    page = urlopen(quote_page);
+except:
+    print("Kunde inte öppna URL");
+    exit()
 soup = BeautifulSoup(page, "html.parser");
 name = soup.find("h1", attrs={"class" : "large marginBottom10px"});
 name = name.text.strip();
@@ -23,6 +35,10 @@ price = price.text.strip();
 print(name + ": NAV "+ price);
 
 price = str(convertString(price)) + "kr";
-with open("D:\program\python\index.csv", "a") as csv_file:
-    writer = csv.writer(csv_file, delimiter=";");
-    writer.writerow([name, price, datetime.now().strftime("20%y/%m/%d - %H:%M")]);
+try:
+    with open(csv_path, "a") as csv_file:
+        writer = csv.writer(csv_file, delimiter=";");
+        writer.writerow([name, price, datetime.now().strftime("20%y/%m/%d - %H:%M")]);
+except:
+    print("Kunde inte spara filen!")
+    exit()
